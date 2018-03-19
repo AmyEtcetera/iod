08fdd965e4c7db0a0f499529a72ea40f65d8a2d8
Amy L Letson <asemenuk@temple.edu>
2018-03-19 12:13:55 -0400

    minimal scss comments & spacing; add script to app.js to resolve jump link prob with sticky nav covering top of area.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -8,10 +8,28 @@ import Foundation from 'foundation-sites';
 // the line below
 //import './lib/foundation-explicit-pieces';
 
-
 $(document).foundation();
 
 //  test here:
 // $(function(){
 //      $("p").css("border", "3px solid blue");
 // });
+
+
+// This is for offsetting the problem created by the sticky nav when using jump links:
+// The function actually applying the offset
+function offsetAnchor() {
+  if (location.hash.length !== 0) {
+    window.scrollTo(window.scrollX, window.scrollY - 60);
+  }
+}
+// Captures click events of all <a> elements with href starting with #
+$(document).on('click', 'a[href^="#"]', function(event) {
+  // Click events are captured before hashchanges. Timeout
+  // causes offsetAnchor to be called after the page jump.
+  window.setTimeout(function() {
+    offsetAnchor();
+  }, 0);
+});
+// Set the offset when entering page with hash present in the url
+window.setTimeout(offsetAnchor, 0);
